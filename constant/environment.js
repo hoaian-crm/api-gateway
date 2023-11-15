261a316a7cdfd7de49d0d653e09b0dda05922bff
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2023-11-15 14:51:06 +0700

    Update gitignore

--- constant/environment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppPort = void 0;
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-exports.AppPort = +(process.env.AppPort || 3000);
--- src/modules/permissions/service.ts
+++
@@ -1,6 +0,0 @@
-import { PermissionSchema } from "./schema";
-export class PermissionService {
-  static async getAll() {
-    return PermissionSchema.findAll();
-  }
-}
