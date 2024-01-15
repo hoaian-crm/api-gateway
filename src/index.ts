eed461a6188ae74f391b40985a0307eac1ada2a4
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-01-15 22:02:26 +0700

    Update limit

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,13 @@ const setup = async () => {
   app.use(
     cors({
       origin: "*",
-    })
+    }),
   );
-  app.use(express.json());
+  app.use(express.json({ limit: '50mb' }));
   app.use(
     express.urlencoded({
       extended: true,
+      limit: '50mb'
     })
   );
 
