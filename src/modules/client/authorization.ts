a981e21580963241fdea73ef402aa97deecf0656
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-03-26 10:00:55 +0700

    Disable authorization

--- src/modules/client/authorization.ts
+++ src/modules/client/authorization.ts
@@ -7,31 +7,32 @@ import { PermissionSchema } from "../permissions/schema";
 export const Authorization = (policy: string) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      if (policy === "public") {
-        return next();
-      }
-      const headers = req.headers;
-      const token = headers["authorization"]?.split(" ")[1];
-
-      if (!token) {
-        res.status(401).send("Unauthorized");
-        return;
-      }
-
-      const user: any = jwt.verify(token, Environment.Secret);
-
-      req.headers["UserId"] = user.id;
-      const permissions = await PermissionService.getPermissionOfRole(
-        user.role.id
-      );
-      console.log("permissions is: ", user);
-      if (
-        user.role?.name == Environment.SuperAdminRole ||
-        permissions.map((p) => p.policy).includes(policy)
-      ) {
-        return next();
-      }
-      res.status(403).send("Forbidden Resource");
+      next();
+      // if (policy === "public") {
+      //   return next();
+      // }
+      // const headers = req.headers;
+      // const token = headers["authorization"]?.split(" ")[1];
+      //
+      // if (!token) {
+      //   res.status(401).send("Unauthorized");
+      //   return;
+      // }
+      //
+      // const user: any = jwt.verify(token, Environment.Secret);
+      //
+      // req.headers["UserId"] = user.id;
+      // const permissions = await PermissionService.getPermissionOfRole(
+      //   user.role.id
+      // );
+      // console.log("permissions is: ", user);
+      // if (
+      //   user.role?.name == Environment.SuperAdminRole ||
+      //   permissions.map((p) => p.policy).includes(policy)
+      // ) {
+      //   return next();
+      // }
+      // res.status(403).send("Forbidden Resource");
     } catch (error) {
       console.log("error is", error);
       res.status(401).send("Unauthorized");
